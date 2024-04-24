03ac2df9a98d3d1a16083e3bd0093d7f96ca2ddf
DANYA-PC\User <ulkiora.panda@gmail.com>
2024-04-24 20:43:40 +0500

    added exception handling middleware

---
+++ QueueService.Api/Extensions/ExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using QueueService.Api.Middleware;
+
+namespace QueueService.Api.Extensions
+{
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
---
+++ QueueService.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace QueueService.Api.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ArgumentException ex) when (ex.Message.Contains("Message should not be empty or Message length should not exceed 100 characters."))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Response.ContentType = "application/json";
+
+                var errorResponse = new { error = "Message should not be empty or Message length should not exceed 100 characters." };
+                var jsonErrorResponse = JsonConvert.SerializeObject(errorResponse);
+
+                await context.Response.WriteAsync(jsonErrorResponse);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var errorResponse = new { error = "An unexpected error occurred." };
+                var jsonErrorResponse = JsonConvert.SerializeObject(errorResponse);
+
+                await context.Response.WriteAsync(jsonErrorResponse);
+            }
+        }
+    }
+}
